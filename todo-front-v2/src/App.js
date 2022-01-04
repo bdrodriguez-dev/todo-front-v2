3555be4526774d8c378eca086f530a4671aa4fc9
Braulio Rodriguez <bdrodriguez.dev@gmail.com>
2022-01-04 12:11:24 -0500

    Todos load from backend. Displayed and added a little styling.

--- todo-front-v2/src/App.js
+++ todo-front-v2/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+    const [showTaskForm, setShowTaskForm] = useState(false);
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+        const getTasks = async () => {
+            try {
+                const tasks = await axios.get(`http://localhost:8000/todos`);
+                setTasks(tasks.data);
+                console.log(tasks.data);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        getTasks();
+    }, []);
+
+    const toggleShowTaskForm = () => {
+        setShowTaskForm(!showTaskForm);
+    };
+
+    return <>
+        {tasks.map(task => {
+            return <ul
+                key={task._id}
+                className={`mx-6 my-2 shadow-md p-4`}
+            >
+                <li className={`text-xl font-bold`}>{task.todo}</li>
+                <li>{task.list} - {task.dueDate}</li>
+            </ul>;
+        })}
+        <button className={`text-3xl`} onClick={toggleShowTaskForm}>New task</button>
+        {showTaskForm ? <form className={`border-2 border-red-900`}>
+            <input type="text" className={``} />
+        </form> : null }
+    </>;
 }
 
 export default App;
