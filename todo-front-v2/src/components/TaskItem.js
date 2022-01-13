a31062cf4212fc1f8991d056bf0104c3bb5e4d3f
Braulio Rodriguez <bdrodriguez.dev@gmail.com>
2022-01-13 16:03:11 -0500

    Removed taskDrawer

--- todo-front-v2/src/components/TaskItem.js
+++ todo-front-v2/src/components/TaskItem.js
@@ -23,41 +23,24 @@ const TaskItem = ({taskObj, deleteFunc, showEditFormTrue, onDescriptionChange, t
                 editInputValue={editInputValue}
             />
             :
-            <div>
-                <div className={`flex`}>
-                    <div className={`w-full p-4 flex-col`}>
-                        <p className={`text-xl font-bold`}>
-                            {taskObj?.todo}
-                        </p>
-                        <p>
-                            {taskObj?.list} - {taskObj?.dueDate}
-                        </p>
-                    </div>
-                    <div className={`w-[100px] flex justify-center items-center`}>
-                        <button onClick={toggleShowTaskDrawer}
-                                className={`h-9 w-9 flex justify-center items-center`}>
-                            <InformationCircleIcon className={`h-9 w-9 text-blue-500`}/>
-                        </button>
-                    </div>
+
+            <div className={`flex`}>
+                <div className={`w-full p-4 flex-col`}>
+                    <p className={`text-xl font-bold`}>
+                        {taskObj?.todo}
+                    </p>
+                    <p>
+                        {taskObj?.list} - {taskObj?.dueDate}
+                    </p>
                 </div>
-
-
-                {showTaskDrawer ?
-                    <div
-                        className={`flex justify-center`}>
-                        <button
-                            onClick={() => deleteFunc(taskObj._id)}
-                            className={`bg-red-600 text-white m-4 p-2 rounded-md`}
-                        >Delete
-                        </button>
-                        <button
-                            className={`bg-blue-600 text-white m-4 p-2 rounded-md`}
-                            onClick={() => setShowEditForm(true)}
-                        >
-                            Edit
-                        </button>
-                    </div> : null}
-            </div>}
+                <div className={`w-[100px] flex justify-center items-center`}>
+                    <button onClick={toggleShowTaskDrawer}
+                            className={`h-9 w-9 flex justify-center items-center`}>
+                        <InformationCircleIcon className={`h-9 w-9 text-blue-500`}/>
+                    </button>
+                </div>
+            </div>
+        }
 
     </>
 
